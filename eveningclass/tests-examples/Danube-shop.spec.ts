f836a3f90f28ccd37a7b370c8aa92f063f50db6a
SaradhaPalanikumar <110985488+SaradhaPalanikumar@users.noreply.github.com>
2022-09-28 13:44:34 +0530

    Update Danube-shop.spec.ts

--- eveningclass/tests-examples/Danube-shop.spec.ts
+++ eveningclass/tests-examples/Danube-shop.spec.ts
@@ -46,26 +46,61 @@ test('Add to Cart', async ({page}) =>
 {
       
     await page.click('text=Haben oder haben');
+
     await page.click('text=add to cart');
+
+    //Assert if item is added to cart
     await expect(page.locator('xpath=//*[@id="app-content"]/div/div/ul/li')).toContainText("1x, Haben oder haben, Fric Eromm, $9.95");
-    await expect(page.locator('ul > li')).toBeNaN;
+
+    //Assert the total price to be a number
+    await expect(page.locator('#total-price')).toBeNaN;
+
+    //Assert that the checkbox is visible 
+    await expect(page.locator('#billing-coupon')).toBeVisible();
+
+    await page.check('#billing-coupon');
+
+    //Assert checkbox is checked
+    expect(await page.isChecked('#billing-coupon')).toBeTruthy();
+
+    //Assert for text
+    await expect(page.locator('xpath=//*[@id="app-content"]/div/div/div[2]/p')).toContainText("Invalid coupon.");
+
+    await page.click('text=Empty Cart');
+
+    //Assert if cart is empty
+    await expect(page.locator('xpath=//*[@id="app-content"]/div/div/ul')).toBeEmpty;
+    await expect(page.locator('#total-price')).toHaveId('total-price');
+
     await page.click('text=Checkout');
+
+    //Assert checkout page
+    await expect(page.locator('xpath=//*[@id="app-content"]/div/div/h1')).toHaveText("Checkout");
+
+
     await page.fill('#s-name', "Saradha Mohan"); 
+
     await page.fill('[placeholder="Surname"]', "Saradha");
+
     await page.fill('#s-address', "G-1,ABC Appartment,cantonment"); 
+
     await page.fill('#s-zipcode',"620001");
+
     await page.fill('#s-city',"Chennai");
+
     await page.fill('[placeholder="Company (optional)"]',"SSR");
+
     await page.check('#asap');
+
     await page.click('xpath=//*[@id="app-content"]/div/div/button');
+
+    //Assert to have text
     await expect(page.locator('#order-confirmation')).toHaveText('All good, order is on the way. Thank you!!');
 
 
    
 
 })
-    
-
 
 
    
